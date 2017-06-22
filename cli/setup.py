87fb8adfd216edcb6a3ac4d090c28fbdccbd16fe
Dmitry Ivanov <divanov@satori.com>
2017-06-22 15:54:42 +0200

    Bump CLI version

--- cli/setup.py
+++ cli/setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(name='satori-rtm-cli',
-      version='1.0.0',
+      version='1.0.1',
       description='Satori RTM CLI',
       author='Satori',
       author_email='sdk@satori.com',
       url='http://www.satori.com/',
       scripts=['satori_rtm_cli.py'],
-      install_requires=['satori-rtm-sdk >=1.0.4', 'docopt'])
+      install_requires=['satori-rtm-sdk >=1.2.0', 'docopt'])
