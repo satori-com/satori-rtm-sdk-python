0635891eab739f410d0973cca6b09223f960907e
Dmitry Ivanov <divanov@satori.com>
2017-07-06 18:33:25 -0700

    Make tests faster

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -2,8 +2,22 @@ from __future__ import print_function
 
 import os
 import sys
+import time
 import unittest
 from test.utils import print_resource_usage
+
+class TimedResult(unittest.TextTestResult):
+    def startTest(self, test):
+        self._origin = time.time()
+        unittest.TextTestResult.startTest(self, test)
+
+    def addSuccess(self, test):
+        duration = time.time() - self._origin
+        if duration > 1:
+            name = self.getDescription(test)
+            print('\nSlow test {0}: {1:.02} sec'.format(name, duration))
+        unittest.TextTestResult.addSuccess(self, test)
+
 
 if __name__ == '__main__':
 
@@ -17,7 +31,8 @@ if __name__ == '__main__':
                     locals()[name] = value
 
     try:
-        unittest.main()
+        unittest.main(
+            testRunner=unittest.TextTestRunner(resultclass=TimedResult))
     finally:
         print_resource_usage()
         sys.stdout.flush()
