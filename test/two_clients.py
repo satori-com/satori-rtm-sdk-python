550315e6a28f24cea40d4bc214d203477ed01051
Dmitry Ivanov <divanov@satori.com>
2017-12-20 16:13:31 +0100

    Satisfy linter

--- test/two_clients.py
+++ test/two_clients.py
@@ -211,11 +211,14 @@ class TestTwoClients(unittest.TestCase):
                 got_messages = so.extract_received_messages()
 
                 self.assertEqual(got_messages, message_list + ['finalizer'])
-    
+
     def test_two_clients_with_deduplication(self):
         with make_client(endpoint=endpoint, appkey=appkey) as pub:
             with make_client(endpoint=endpoint, appkey=appkey) as sub:
-                so = sync_subscribe(sub, channel, args={'only': 'value_changes'})
+                so = sync_subscribe(
+                    sub,
+                    channel,
+                    args={'only': 'value_changes'})
                 pub.publish(channel, "first")
                 so.wait_for_channel_data()
                 for _ in range(10):
